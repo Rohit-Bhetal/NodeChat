bb832781be6bdd14d47852146d2b0a00d3552f80
Rohit Bhetal <96344296+Rohit-Bhetal@users.noreply.github.com>
2023-12-27 00:28:18 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "20902351-4a63-4841-9b73-affd2e842164" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    } else {
+      // Handle errors without a response object
+      console.error(e); // Log the error for debugging
+      return res.status(500).json({ error: "Something went wrong" });
+    }
+  }
+});
+
+app.listen(3001);
